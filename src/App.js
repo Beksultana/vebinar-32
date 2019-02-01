4d3923c2646a57754a877ab3ca8c58bcc5081652
sultan <aidaraliuulu@gmail.com>
2019-02-01 15:04:44 +0600

    Создал файл containers и components. Внутри файла containers создал js файл MainContainer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import {Switch, Route} from "react-router-dom";
+import MainContainer from "./containers/MainContainer/MainContainer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Switch>
+        <Route path='/' exact component={MainContainer}/>
+      </Switch>
     );
   }
 }
---
+++ src/containers/MainContainer/MainContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <div className={'MainContainer'}>
+                <h1>Main Container</h1>
+            </div>
+        );
+    }
+}
+
+export default MainContainer;
